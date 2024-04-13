29192125eda595bdd8a2bfadf4f546eb3808910b
yushiang <tsengyushiang@gmail.com>
2024-04-13 17:23:32 +0800

    feature: initialize dnd tree view.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,196 @@
-import logo from './logo.svg';
-import './App.css';
+import { v4 as uuidv4 } from "uuid";
+import {
+  DndProvider,
+  getBackendOptions,
+  getDescendants,
+  MultiBackend,
+  Tree,
+} from "@minoru/react-dnd-treeview";
+import { NativeTypes } from "react-dnd-html5-backend";
+import React from "react";
+import Node from "./Node";
+import Placeholder from "./Placeholder";
+import useTreeOpenHandler from "./useTreeOpenHandler";
+import styles from "./styles.module.css";
+import { ExternalNode } from "./ExternalNode.js";
 
-function App() {
+const getLastId = (treeData) => {
+  const reversedArray = [...treeData].sort((a, b) => {
+    if (a.id < b.id) {
+      return 1;
+    } else if (a.id > b.id) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  if (reversedArray.length > 0) {
+    return reversedArray[0].id;
+  }
+
+  return 0;
+};
+
+const reorderArray = (array, sourceIndex, targetIndex) => {
+  const newArray = [...array];
+  const element = newArray.splice(sourceIndex, 1)[0];
+  newArray.splice(targetIndex, 0, element);
+  return newArray;
+};
+
+export default function App() {
+  const { ref, getPipeHeight, toggle } = useTreeOpenHandler();
+  const [tree, setTree] = React.useState([]);
+  const handleDrop = (_, options) => {
+    const { dropTargetId, destinationIndex, monitor } = options;
+
+    const { start, end, treeData, dragSourceId } = (() => {
+      const treeData = [...tree];
+      let { dragSourceId } = options;
+
+      const itemType = monitor.getItemType();
+      if (itemType === NativeTypes.TEXT) {
+        const nodeJson = monitor.getItem().text;
+        const node = JSON.parse(nodeJson);
+        node.id = uuidv4();
+        node.parent = 0;
+        dragSourceId = node.id;
+        treeData.push(node);
+      }
+
+      const start = treeData.find((v) => v.id === dragSourceId);
+      const end = treeData.find((v) => v.id === dropTargetId);
+      return { start, end, treeData, dragSourceId };
+    })();
+
+    if (
+      start?.parent === dropTargetId &&
+      start &&
+      typeof destinationIndex === "number"
+    ) {
+      const output = reorderArray(
+        treeData,
+        treeData.indexOf(start),
+        destinationIndex
+      );
+      setTree(output);
+    }
+
+    if (
+      start?.parent !== dropTargetId &&
+      start &&
+      typeof destinationIndex === "number"
+    ) {
+      if (
+        getDescendants(treeData, dragSourceId).find(
+          (el) => el.id === dropTargetId
+        ) ||
+        dropTargetId === dragSourceId ||
+        (end && !end?.droppable)
+      )
+        return;
+
+      const output = reorderArray(
+        treeData,
+        treeData.indexOf(start),
+        destinationIndex
+      );
+      const movedElement = output.find((el) => el.id === dragSourceId);
+      if (movedElement) movedElement.parent = dropTargetId;
+      setTree(output);
+    }
+  };
+
+  const externalNodes = [
+    {
+      id: "text",
+
+      text: "component",
+    },
+    {
+      id: "dropMenu",
+      droppable: true,
+      text: "container",
+    },
+  ];
+
+  const handleDelete = (id) => {
+    const deleteIds = [id, ...getDescendants(tree, id).map((node) => node.id)];
+    const newTree = tree.filter((node) => !deleteIds.includes(node.id));
+    setTree(newTree);
+  };
+
+  const handleCopy = (id) => {
+    const lastId = getLastId(tree);
+    const targetNode = tree.find((n) => n.id === id);
+    const descendants = getDescendants(tree, id);
+    const partialTree = descendants.map((node) => ({
+      ...node,
+      id: node.id + lastId,
+      parent: node.parent + lastId,
+    }));
+
+    setTree([
+      ...tree,
+      {
+        ...targetNode,
+        id: targetNode.id + lastId,
+      },
+      ...partialTree,
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {externalNodes.map((node) => (
+          <ExternalNode key={node.id} node={node} />
+        ))}
+      </div>
+      <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+        <div className={styles.wrapper}>
+          <Tree
+            ref={ref}
+            classes={{
+              root: styles.treeRoot,
+              placeholder: styles.placeholder,
+              dropTarget: styles.dropTarget,
+              listItem: styles.listItem,
+            }}
+            tree={tree}
+            extraAcceptTypes={[NativeTypes.TEXT]}
+            sort={false}
+            rootId={0}
+            insertDroppableFirst={false}
+            enableAnimateExpand={true}
+            onDrop={handleDrop}
+            canDrop={() => true}
+            dropTargetOffset={5}
+            placeholderRender={(node, { depth }) => (
+              <Placeholder node={node} depth={depth} />
+            )}
+            render={(node, { depth, isOpen, isDropTarget, onToggle }) => (
+              <Node
+                onDelete={handleDelete}
+                onCopy={handleCopy}
+                getPipeHeight={getPipeHeight}
+                node={node}
+                depth={depth}
+                isOpen={isOpen}
+                onToggle={onToggle}
+                onClick={() => {
+                  if (node.droppable) {
+                    toggle(node?.id);
+                  }
+                }}
+                isDropTarget={isDropTarget}
+                treeData={tree}
+              />
+            )}
+          />
+        </div>
+      </DndProvider>
     </div>
   );
 }
-
-export default App;
